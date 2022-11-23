cec71c2c657df5325e8a6d07ac60acd0fdefbaf0
AGunTang <tanggun@laborsing.com>
2022-11-23 11:17:44 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx'
+
+console.log('test 1.0.0')
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />);
